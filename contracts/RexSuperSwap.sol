18573bf437e2a6c39145f856e97a4acebf8b8649
Shreyas Papinwar <spapinwar@gmail.com>
2022-06-15 18:16:18 +0100

    Adding support for non underlaying tokens and solve the decimal issue

--- contracts/RexSuperSwap.sol
+++ contracts/RexSuperSwap.sol
@@ -1,104 +1,124 @@
 //SPDX-License-Identifier: MIT
- pragma solidity ^0.7.6;
- pragma abicoder v2;
+pragma solidity ^0.7.6;
+pragma abicoder v2;
 
- import {ISuperToken} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
- import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
- import "@uniswap/swap-router-contracts/contracts/interfaces/ISwapRouter02.sol";
+import {ISuperToken} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/swap-router-contracts/contracts/interfaces/ISwapRouter02.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
- contract RexSuperSwap {
-     ISwapRouter02 public immutable swapRouter;
+contract RexSuperSwap {
+    ISwapRouter02 public immutable swapRouter;
 
-     event SuperSwapComplete(uint256 amountOut);
+    event SuperSwapComplete(uint256 amountOut);
 
-     constructor(ISwapRouter02 _swapRouter) {
-         swapRouter = _swapRouter;
-     }
+    constructor(ISwapRouter02 _swapRouter) {
+        swapRouter = _swapRouter;
+    }
 
-     /**
-      * @dev Swaps `amountIn` of `_from` SuperToken for at least `amountOutMin`
-      * of `_to` SuperToken through `path` with `poolFees` fees for each pair.
-      *
-      * Returns the amount of `_to` SuperToken received.
-      */
-     function swap(
-         ISuperToken _from,
-         ISuperToken _to,
-         uint256 amountIn,
-         uint256 amountOutMin,
-         address[] memory path,
-         uint24[] memory poolFees // Example: 0.3% * 10000 = 3000
-     ) external returns (uint256 amountOut) {
-         require(amountIn > 0, "Amount cannot be 0");
-         require(path.length > 1, "Incorrect path");
-         require(
-             poolFees.length == path.length - 1,
-             "Incorrect poolFees length"
-         );
+    function approve(address _token, address _spender) internal {
+        IERC20 token = IERC20(_token);
+        if (_token.allowance(_spender, msg.sender) == 0) {
+            // Unlimited approval, as contract is not storing any tokens
+            TransferHelper.safeApprove(address(_token), address(_spender), (10 ** 56));
+        }
+    }
 
-         // Step 1: Get underlying tokens and verify path
-         address fromBase = _from.getUnderlyingToken();
-         address toBase = _to.getUnderlyingToken();
+    /**
+    * @dev Swaps `amountIn` of `_from` SuperToken for at least `amountOutMin`
+    * of `_to` SuperToken through `path` with `poolFees` fees for each pair.
+    *
+    * Returns the amount of `_to` SuperToken received.
+    */
+    function swap(
+        ISuperToken _from,
+        ISuperToken _to,
+        uint256 amountIn,
+        uint256 amountOutMin, // Use underlaying decimals
+        address[] memory path,
+        uint24[] memory poolFees, // Example: 0.3% * 10000 = 3000
+        bool _hasUnderlyingFrom,
+        bool _hasUnderlyingTo
+    ) external returns (uint256 amountOut) {
+        require(amountIn > 0, "Amount cannot be 0");
+        require(path.length > 1, "Incorrect path");
+        require(
+            poolFees.length == path.length - 1,
+            "Incorrect poolFees length"
+        );
 
-         require(path[0] == fromBase, "Invalid 'from' base token");
-         require(path[path.length - 1] == toBase, "Invalid 'to' base token");
+        // Step 1: Get underlying tokens and verify path
+        address fromBase;
+        if (_hasUnderlyingFrom) {
+            fromBase = _from.underlying();
+        } else {
+            fromBase = _from;
+        }
 
-         // Step 2: Transfer SuperTokens from sender
-         TransferHelper.safeTransferFrom(
-             address(_from),
-             msg.sender,
-             address(this),
-             amountIn
-         );
+        address toBase;
+        if (_hasUnderlyingTo) {
+            toBase = _to.underlying();
+        } else {
+            toBase = _to;
+        }
 
-         // Step 3: Downgrade
-         _from.downgrade(amountIn);
+        require(path[0] == fromBase, "Invalid 'from' base token");
+        require(path[path.length - 1] == toBase, "Invalid 'to' base token");
 
-         // Step 4: Approve and Swap
+        // Step 2: Transfer SuperTokens from sender
+        TransferHelper.safeTransferFrom(
+            address(_from),
+            msg.sender,
+            address(this),
+            amountIn
+        );
 
-         // Encode the path for swap
-         bytes memory encodedPath;
-         for (uint256 i = 0; i < path.length; i++) {
-             if (i == path.length - 1) {
-                 encodedPath = abi.encodePacked(encodedPath, path[i]);
-             } else {
-                 encodedPath = abi.encodePacked(
-                     encodedPath,
-                     path[i],
-                     poolFees[i]
-                 );
-             }
-         }
+        // Step 3: Downgrade
+        _from.downgrade(amountIn);
 
-         // Approve the router to spend token supplied (fromBase).
-         TransferHelper.safeApprove(
-             fromBase,
-             address(swapRouter),
-             amountIn
-         );
+        // Step 4: Approve and Swap
 
-         IV3SwapRouter.ExactInputParams memory params = IV3SwapRouter
-             .ExactInputParams({
-                 path: encodedPath,
-                 recipient: address(this),
-                 amountIn: amountIn,
-                 amountOutMinimum: amountOutMin
-             });
+        // Encode the path for swap
+        bytes memory encodedPath;
+        for (uint256 i = 0; i < path.length; i++) {
+            if (i == path.length - 1) {
+                encodedPath = abi.encodePacked(encodedPath, path[i]);
+            } else {
+                encodedPath = abi.encodePacked(
+                    encodedPath,
+                    path[i],
+                    poolFees[i]
+                );
+            }
+        }
 
-         // Execute the swap
-         amountOut = swapRouter.exactInput(params);
+        // Approve the router to spend token supplied (fromBase).
+        approve(fromBase, address(swapRouter));
 
-         // Step 5: Upgrade and send tokens back
-         TransferHelper.safeApprove(address(toBase), address(_to), amountOut);
-         _to.upgrade(amountOut);
-         TransferHelper.safeApprove(address(_to), msg.sender, amountOut);
-         TransferHelper.safeTransferFrom(
-             address(_to),
-             address(this),
-             msg.sender,
-             amountOut
-         );
+        IV3SwapRouter.ExactInputParams memory params = IV3SwapRouter
+            .ExactInputParams({
+                path: encodedPath,
+                recipient: address(this),
+                amountIn: fromBase.balanceOf(address(this)),
+                amountOutMinimum: amountOutMin
+            });
 
-         emit SuperSwapComplete(amountOut);
-     }
+        // Execute the swap
+        amountOut = swapRouter.exactInput(params);
+
+        uint256 toBaseAmount = toBase.balanceOf(address(this));
+
+        // Step 5: Upgrade and send tokens back
+        approve(address(toBase), address(_to));
+        _to.upgrade(toBaseAmount);
+        approve(address(_to), msg.sender);
+        TransferHelper.safeTransferFrom(
+            address(_to),
+            address(this),
+            msg.sender,
+            _to.balanceOf(address(this));
+        );
+
+        emit SuperSwapComplete(amountOut);
+    }
  }
